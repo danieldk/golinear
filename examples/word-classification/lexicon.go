39f4d97c3cdebbcf43e83735740a62f1fa299586
Daniël de Kok <me@danieldk.eu>
2012-09-13 09:28:40 +0200

    WIP example: word classification.

---
+++ examples/word-classification/lexicon.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"strconv"
+	"strings"
+)
+
+type Dictionary map[string]map[string]uint64
+
+func readDictionary(r *bufio.Reader) Dictionary {
+	dictionary := make(Dictionary)
+
+	l, e := ReadLn(r)
+	for e == nil {
+		parts := strings.Split(l, " ")
+
+		word := parts[0]
+		dictionary[word] = make(map[string]uint64)
+
+		tagsCounts := parts[1:]
+
+		for i := 0; i < len(tagsCounts); i += 2 {
+			freq, err := strconv.ParseUint(tagsCounts[i+1], 10, 64)
+			if err != nil {
+				panic(err)
+			}
+
+			dictionary[word][tagsCounts[i]] = freq
+		}
+
+		l, e = ReadLn(r)
+	}
+
+	return dictionary
+}
---
+++ examples/word-classification/util.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+)
+
+func ReadLn(r *bufio.Reader) (string, error) {
+	var (
+		isPrefix bool  = true
+		err      error = nil
+		line, ln []byte
+	)
+	for isPrefix && err == nil {
+		line, isPrefix, err = r.ReadLine()
+		ln = append(ln, line...)
+	}
+	return string(ln), err
+}
